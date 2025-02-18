5a152511a3432c465ad6482822405d09dee218ca
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-02-18 18:48:58 -0300

    chore:Estruturação de pastas

---
+++ src/main/java/com/hav/imobiliaria/controller/ImovelController.java
@@ -0,0 +1,9 @@
+package com.hav.imobiliaria.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("imoveis")
+public class ImovelController {
+}
