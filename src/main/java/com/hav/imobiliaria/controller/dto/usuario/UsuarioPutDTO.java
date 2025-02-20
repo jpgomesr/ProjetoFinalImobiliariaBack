f19fe12dd643f2cc5b9ade23ca2361411f099cf5
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-02-20 17:00:26 -0300

    chore: Funcionalidades relacionadas ao imóvel feitas, resta somente as confirmações do dto

--- src/main/java/com/hav/imobiliaria/controller/dto/usuario/UsuarioPutDTO.java
+++ src/main/java/com/hav/imobiliaria/controller/dto/usuario/UsuarioPutDTO.java
@@ -1,11 +1,12 @@
 package com.hav.imobiliaria.controller.dto.usuario;
 
 public record UsuarioPutDTO(
-            Long id,
-            String role,
-            String nome,
-            String telefone,
-            String email,
-            String descricao,
-            String foto
-) {}
+        Long id,
+        String role,
+        String nome,
+        String telefone,
+        String email,
+        String descricao,
+        String foto
+) {
+}
