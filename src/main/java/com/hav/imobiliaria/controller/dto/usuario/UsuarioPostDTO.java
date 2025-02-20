84ab8ad88e7978148bc4708e97254d6cfd6fb681
João Pedro Scheid <142051698+JoaoScheid@users.noreply.github.com>
2025-02-20 19:43:17 -0300

    chore: Terminando de colocar as etiquetas de usuario dto

--- src/main/java/com/hav/imobiliaria/controller/dto/usuario/UsuarioPostDTO.java
+++ src/main/java/com/hav/imobiliaria/controller/dto/usuario/UsuarioPostDTO.java
@@ -12,9 +12,14 @@ public record UsuarioPostDTO(
         @Size(max = 45, message = "O email deve conter até 45 caracteres")
         @NotBlank(message = "O email é obrigatório")
         String email,
+        @Size(max = 30, message = "A senha deve conter até 30 caracteres")
+        @NotBlank(message = "A senha é obrigatória")
         String senha,
+        @Pattern(regexp = "^[0-9]{11}$", message = "O telefone deve conter exatamente 11 dígitos numéricos")
         String telefone,
+        @Size(max = 500, message = "A mensagem deve conter até 500 caracteres")
         String descricao,
+        @NotBlank(message = "A role é obrigatória")
         String role,
         String foto
 ) {
