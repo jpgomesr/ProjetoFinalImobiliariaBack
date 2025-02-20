ae24e0bf61b17a6be58edc4e24457dc2109bb960
João Pedro Scheid <142051698+JoaoScheid@users.noreply.github.com>
2025-02-20 17:00:27 -0300

    chore: Funcionalidades relacionadas ao dto de proprietário e anotações de  proprietario e usuario

--- src/main/java/com/hav/imobiliaria/controller/dto/usuario/UsuarioPostDTO.java
+++ src/main/java/com/hav/imobiliaria/controller/dto/usuario/UsuarioPostDTO.java
@@ -1,7 +1,16 @@
 package com.hav.imobiliaria.controller.dto.usuario;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record UsuarioPostDTO(
+        @Size(max = 45, message = "O nome deve conter até 45 caracteres")
+        @NotBlank(message = "O nome é obrigatório")
+        @Pattern(regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$", message = "O nome deve conter apenas letras e espaços")
         String nome,
+        @Size(max = 45, message = "O email deve conter até 45 caracteres")
+        @NotBlank(message = "O email é obrigatório")
         String email,
         String senha,
         String telefone,
