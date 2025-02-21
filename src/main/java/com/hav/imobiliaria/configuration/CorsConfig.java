7347a32e009e594f734b9f433c2e48b21bd249d5
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-02-21 15:49:47 -0300

    feat:Cors config test

---
+++ src/main/java/com/hav/imobiliaria/configuration/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.hav.imobiliaria.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Remova ou comente essa linha para desabilitar o CORS global
+        registry.addMapping("/**").
+                allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+
+    }
+
+}
