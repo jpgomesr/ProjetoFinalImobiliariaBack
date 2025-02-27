ccf15f3ea557e5641e7a31f4537d9ab733cdd787
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-02-27 16:15:10 -0300

    feat:Excluindo fotos no s3 ao excluir imóvel

--- src/main/java/com/hav/imobiliaria/service/S3Service.java
+++ src/main/java/com/hav/imobiliaria/service/S3Service.java
@@ -19,7 +19,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Service
 public class S3Service {
@@ -80,5 +82,15 @@ public class S3Service {
         this.s3Client.deleteObject(builder -> builder.bucket(this.bucketName).key(key));
 
     }
+    public void excluirObjeto(List<String> urls){
+        Stream<String> urlsFormatadas =
+                urls.stream().map(url ->
+                        url.replace("https://hav-bucket-c.s3.amazonaws.com/", ""));
+
+        urlsFormatadas.forEach
+                (url ->
+                        this.s3Client.deleteObject(builder -> builder.bucket(this.bucketName).key(url)));
+    }
+
 
 }
