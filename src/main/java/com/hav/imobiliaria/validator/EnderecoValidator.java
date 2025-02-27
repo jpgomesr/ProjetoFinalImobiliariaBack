8c31027d63787d7b8ed1cfbe4d9a123ed298b100
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-02-26 21:50:08 -0300

    chore: Novas exceptions criadas e dois validators feitos

---
+++ src/main/java/com/hav/imobiliaria/validator/EnderecoValidator.java
@@ -0,0 +1,10 @@
+package com.hav.imobiliaria.validator;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class EnderecoValidator {
+    
+}
