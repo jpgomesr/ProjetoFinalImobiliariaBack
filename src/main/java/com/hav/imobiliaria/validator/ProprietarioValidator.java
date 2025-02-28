428949092e62117e8a7759a73b783fd6a0e77b59
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-02-28 14:43:44 -0300

    chore: modificações nas validações feitas, se necessário ocorrerão mudanças

--- src/main/java/com/hav/imobiliaria/validator/ProprietarioValidator.java
+++ src/main/java/com/hav/imobiliaria/validator/ProprietarioValidator.java
@@ -1,10 +1,14 @@
 package com.hav.imobiliaria.validator;
 
+import com.hav.imobiliaria.exceptions.CPFJaCadastradoException;
+import com.hav.imobiliaria.exceptions.TelefoneJaCadastradoException;
 import com.hav.imobiliaria.exceptions.UsuarioJaCadastradoException;
 import com.hav.imobiliaria.model.Proprietario;
 import com.hav.imobiliaria.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @AllArgsConstructor
@@ -12,12 +16,24 @@ public class ProprietarioValidator {
     private ProprietarioRepository repository;
 
     public void validar(Proprietario proprietario){
-        if (existeProprietarioCadastrado(proprietario)){
-            throw new UsuarioJaCadastradoException();
+        if (existeTelefoneCadastrado(proprietario)){
+            throw new TelefoneJaCadastradoException();
+        }
+        if (existeCPFCadastrado(proprietario)){
+            throw new CPFJaCadastradoException();
         }
     }
 
-    private boolean existeProprietarioCadastrado(Proprietario proprietario){
-        return repository.existsById(proprietario.getId());
+    private boolean existeTelefoneCadastrado(Proprietario proprietario){
+
+        Optional<Proprietario> proprietario1 = this.repository.findByTelefone(proprietario.getTelefone());
+        return proprietario1.isEmpty() || !proprietario1.get().getId().equals(proprietario.getId());
     }
+
+    private boolean existeCPFCadastrado(Proprietario proprietario){
+
+        Optional<Proprietario> proprietario1 = this.repository.findByCpf(proprietario.getCpf());
+        return proprietario1.isEmpty() || !proprietario1.get().getId().equals(proprietario.getId());
+    }
+
 }
