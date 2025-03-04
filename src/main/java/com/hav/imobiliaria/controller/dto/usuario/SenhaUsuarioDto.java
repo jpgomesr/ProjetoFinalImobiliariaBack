7898511fbe7b8c334fc10d76fc6a7a5fd3224986
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-03-03 21:41:45 -0300

    feat:Alterado o update para senha e criado um patch para alterar a senha

---
+++ src/main/java/com/hav/imobiliaria/controller/dto/usuario/SenhaUsuarioDto.java
@@ -0,0 +1,11 @@
+package com.hav.imobiliaria.controller.dto.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SenhaUsuarioDto(
+        @Size(min = 8, max = 45, message = "A senha deve conter entre 8 a 45 caractéres")
+        @NotBlank(message = "A senha é obrigatória")
+        String senha
+) {
+}
