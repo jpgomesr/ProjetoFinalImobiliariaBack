5e3a74522b02c06f9d8b2c77ed9ef7a37421d228
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-03-18 16:59:44 -0300

    feat: Horarios do corretor

--- src/main/java/com/hav/imobiliaria/model/entity/Agendamento.java
+++ src/main/java/com/hav/imobiliaria/model/entity/Agendamento.java
@@ -2,6 +2,8 @@ package com.hav.imobiliaria.model.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "agendamento")
 public class Agendamento {
@@ -9,6 +11,21 @@ public class Agendamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private LocalDateTime dataHora;
+
+    @OneToOne
+    @JoinColumn(name = "id_imovel")
+    private Imovel imovel;
+
+    @ManyToOne
+    @JoinColumn(name = "id_corretor")
+    private Corretor corretor;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private UsuarioComum usuarioComum;
 
     
 }
