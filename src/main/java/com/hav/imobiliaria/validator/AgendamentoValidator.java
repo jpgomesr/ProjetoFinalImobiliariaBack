2abbf55aa02b15298a3f7b19d9cc232ecc124a72
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-03-28 16:58:07 -0300

    feat : Iniciando validação de visita

--- src/main/java/com/hav/imobiliaria/validator/AgendamentoValidator.java
+++ src/main/java/com/hav/imobiliaria/validator/AgendamentoValidator.java
@@ -1,7 +1,10 @@
 package com.hav.imobiliaria.validator;
 
 import com.hav.imobiliaria.controller.dto.agendamento.AgendamentoPostDto;
+import com.hav.imobiliaria.exceptions.requisicao_padrao.AgendamentoJaCadastradoException;
 import com.hav.imobiliaria.model.entity.Agendamento;
+import com.hav.imobiliaria.model.entity.UsuarioComum;
+import com.hav.imobiliaria.model.enums.StatusAgendamentoEnum;
 import com.hav.imobiliaria.repository.AgendamentoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -15,5 +18,15 @@ public class AgendamentoValidator {
     public void validarAgendamento(AgendamentoPostDto agendamentoPostDto) {
 
     }
+    public void verificarAgendamentoImovelUsuario(UsuarioComum usuarioComum, Agendamento agendamento) {
+        if (usuarioComum.getAgendamentos().stream()
+                .anyMatch(agendamento1 ->
+                        agendamento1.getImovel().getId().equals(agendamento.getImovel().getId()) &&
+                                !agendamento1.getStatus().equals(StatusAgendamentoEnum.CONCLUIDO)
+                )) {
+            throw new AgendamentoJaCadastradoException();
+        }
+    }
+
 
 }
