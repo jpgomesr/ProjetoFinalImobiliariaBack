39cbb5b3f8b7050bb76b207872a4c5fbd10f2f9c
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-03-27 21:53:05 -0300

    feat: Finalizado o back-end

--- src/main/java/com/hav/imobiliaria/model/entity/Pergunta.java
+++ src/main/java/com/hav/imobiliaria/model/entity/Pergunta.java
@@ -1,10 +1,7 @@
 package com.hav.imobiliaria.model.entity;
 
 import com.hav.imobiliaria.model.enums.TipoPerguntaEnum;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +14,7 @@ public class Pergunta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "tipo_pergunta")
     private TipoPerguntaEnum tipoPergunta;
     private String email;
     private String telefone;
--- src/main/java/com/hav/imobiliaria/service/PerguntaService.java
+++ src/main/java/com/hav/imobiliaria/service/PerguntaService.java
@@ -3,10 +3,17 @@ package com.hav.imobiliaria.service;
 import com.hav.imobiliaria.controller.dto.pergunta.PerguntaPostDTO;
 import com.hav.imobiliaria.controller.mapper.pergunta.PerguntaPostMapper;
 import com.hav.imobiliaria.model.entity.Pergunta;
+import com.hav.imobiliaria.model.enums.TipoPerguntaEnum;
 import com.hav.imobiliaria.repository.PerguntaRepositry;
+import com.hav.imobiliaria.repository.specs.ImovelSpecs;
+import com.hav.imobiliaria.repository.specs.PerguntaSpecs;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 @AllArgsConstructor
@@ -26,7 +33,31 @@ public class PerguntaService {
         return repositry.save(entity);
     }
 
-    public Pergunta buscarTodasPerguntas(Pageable pageable) {
-        return repositry.findAll(pageable);
+    public Page<Pergunta> pesquisar(
+            TipoPerguntaEnum tipoPergunta,
+            String email,
+            String telefone,
+            String nome,
+            String mensagem,
+            Pageable pageable
+    ) {
+        Specification<Pergunta> specs = Specification.where((root, query, criteriaBuilder) -> criteriaBuilder.conjunction());
+
+        telefone = telefone.replace("(", "")
+                .replace(")", "").replace("-","");
+
+        if (tipoPergunta !=null){
+            specs = specs.and(PerguntaSpecs.tipoPerguntaLike(tipoPergunta));
+        }
+        if (StringUtils.isNotBlank(email)){
+            specs = specs.and(PerguntaSpecs.emailLike(email));
+        }
+        if (StringUtils.isNotBlank(nome)){
+            specs = specs.and(PerguntaSpecs.nomeLike(nome));
+        }
+        if (StringUtils.isNotBlank(telefone)){
+            specs = specs.and(PerguntaSpecs.mensagemLike(mensagem));
+        }
+        return repositry.findAll(specs, pageable);
     }
 }
