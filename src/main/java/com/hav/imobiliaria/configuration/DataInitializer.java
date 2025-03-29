3d23da00c99301ca15864b52413e1bac357b329a
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-03-29 12:01:22 -0300

    feat: Usuarios padroes

--- src/main/java/com/hav/imobiliaria/configuration/DataInitializer.java
+++ src/main/java/com/hav/imobiliaria/configuration/DataInitializer.java
@@ -1,6 +1,8 @@
 package com.hav.imobiliaria.configuration;
 
 import com.hav.imobiliaria.model.entity.Administrador;
+import com.hav.imobiliaria.model.entity.Corretor;
+import com.hav.imobiliaria.model.entity.Editor;
 import com.hav.imobiliaria.model.entity.Usuario;
 import com.hav.imobiliaria.model.enums.RoleEnum;
 import com.hav.imobiliaria.repository.UsuarioRepository;
@@ -18,15 +20,43 @@ public class DataInitializer {
 
     @PostConstruct
     public void init() {
-        if(usuarioRepository.findByEmail("adm@adm.com").isEmpty()) {
+        if(usuarioRepository.findByEmail("adm").isEmpty()) {
             Usuario usuario = new Administrador();
-            usuario.setEmail("adm@adm.com");
+            usuario.setEmail("adm");
             usuario.setSenha(passwordEncoder.encode("adm"));
             usuario.setNome("Adm");
             usuario.setAtivo(true);
             usuario.setRole(RoleEnum.ADMINISTRADOR);
             usuarioRepository.save(usuario);
         }
+        if(usuarioRepository.findByEmail("usu").isEmpty()) {
+            Usuario usuario = new Usuario();
+            usuario.setEmail("usu");
+            usuario.setSenha(passwordEncoder.encode("usu"));
+            usuario.setNome("Usu");
+            usuario.setAtivo(true);
+            usuario.setRole(RoleEnum.USUARIO);
+            usuarioRepository.save(usuario);
+        }
+        if(usuarioRepository.findByEmail("cor").isEmpty()) {
+            Usuario usuario = new Corretor();
+            usuario.setEmail("cor");
+            usuario.setSenha(passwordEncoder.encode("cor"));
+            usuario.setNome("Cor");
+            usuario.setAtivo(true);
+            usuario.setRole(RoleEnum.CORRETOR);
+            usuarioRepository.save(usuario);
+        }
+        if(usuarioRepository.findByEmail("edi").isEmpty()) {
+            Usuario usuario = new Editor();
+            usuario.setEmail("edi");
+            usuario.setSenha(passwordEncoder.encode("edi"));
+            usuario.setNome("Edi");
+            usuario.setAtivo(true);
+            usuario.setRole(RoleEnum.USUARIO);
+            usuarioRepository.save(usuario);
+        }
+
 
     }
 
