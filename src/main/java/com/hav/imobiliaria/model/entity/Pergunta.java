0ae1705fa9a5b59ededa6d8230945d6533078bba
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-04-01 21:51:01 -0300

    feat: Inicio das alterações para a adição das respostas dos editores/adms nos cards de resposta

--- src/main/java/com/hav/imobiliaria/model/entity/Pergunta.java
+++ src/main/java/com/hav/imobiliaria/model/entity/Pergunta.java
@@ -5,12 +5,18 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Pergunta {
+@EntityListeners(AuditingEntityListener.class)
+public class  Pergunta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,6 +26,14 @@ public class Pergunta {
     private String telefone;
     private String nome;
     private String mensagem;
+    @CreatedDate
+    private LocalDateTime data;
+    private String resposta;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Administrador idAdministrador;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Editor idEditor;
 
 
 }
