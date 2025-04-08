d30d40c9b66179e76cd042f8fe7866455ad52f47
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-04-08 16:10:55 -0300

    fix: Arrumando relacionamento 1:1

--- src/main/java/com/hav/imobiliaria/repository/TokenRecuperacaoSenhaRepository.java
+++ src/main/java/com/hav/imobiliaria/repository/TokenRecuperacaoSenhaRepository.java
@@ -4,6 +4,12 @@ import com.hav.imobiliaria.model.entity.TokenRecuperacaoSenha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface TokenRecuperacaoSenhaRepository extends JpaRepository<TokenRecuperacaoSenha, Long> {
+
+
+    Long deleteByUsuario_Id(Long id);
+
 }
