afe064c26b10ed2daee7405c1d94e3dfa597a9bd
Carlos Augusto Kviatkowsky <147559517+CarlosCAK@users.noreply.github.com>
2025-04-17 12:25:49 -0300

    feat: Iniciando authentications

--- src/main/java/com/hav/imobiliaria/security/SecurityConfigurations.java
+++ src/main/java/com/hav/imobiliaria/security/SecurityConfigurations.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,10 +48,17 @@ public class SecurityConfigurations {
                 .build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+        @Bean
+        public AuthenticationManager authenticationManager(
+                GoogleAuthenticationProvider googleAuthenticationProvider
+
+        ) throws Exception {
+
+        
+
+
+            return authenticationConfiguration.getAuthenticationManager();
+        }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
