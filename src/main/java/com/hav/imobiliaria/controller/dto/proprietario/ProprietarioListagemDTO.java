e57f5af6048a6b50a9337b02b8fca662c4e90cb7
Carlos Augusto Kviatkowsky <147559517+CarlosCAK@users.noreply.github.com>
2025-04-21 12:29:11 -0300

    chore: Retornando ativo na busca de proprietarios

--- src/main/java/com/hav/imobiliaria/controller/dto/proprietario/ProprietarioListagemDTO.java
+++ src/main/java/com/hav/imobiliaria/controller/dto/proprietario/ProprietarioListagemDTO.java
@@ -6,6 +6,7 @@ public record ProprietarioListagemDTO(
         String telefone,
         String cpf,
         String email,
-        String imagemUrl
+        String imagemUrl,
+        Boolean ativo
 ) {
 }
